2d00bc55b100699b16216c0d1bcaa61295dedcc4
MaximeGrillet <votre@email.com>
2024-07-19 17:41:09 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return{
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [react()],
+    server: {
+      host: true,
+      watch: {
+        usePolling: true
+      },
+    }
+  }
 })
